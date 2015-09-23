6d9c51562d981d9b89e30c518bd7606271c1b398
dubocr <android.innovation@gmail.com>
2015-09-23 23:51:55 +0200

    Adding rate API parameter Adding collapse option for simple messages

--- src/model/BulkSending.php
+++ src/model/BulkSending.php
@@ -3,22 +3,54 @@ class BulkSending {
 	public $name;
 	public $silent;
 	public $hidden;
+	public $rate = 0;
 	public $messages = array();
 	
+	/**
+	 * $name: name of this bulk (will appear in phone notification)
+	 * $silent: see 'setSilent'
+	 * $hidden: see 'setHiden'
+	 */
 	function __construct($name, $silent = false, $hidden = false) {
 		$this->name = $name;
 		$this->silent = $silent;
 		$this->hidden = $hidden;
 	}
 	
-	public function setSilent($silent) {
+	/**
+	 * Silence sending on the phone (no notification)
+	 * $silent: boolean.
+	 * true: No notification
+	 * false: Each sending notified
+	 */
+	 public function setSilent($silent) {
 		$this->silent = $silent;
 	}
 	
-	public function setHidden($hidden) {
+	/**
+	 * Hide sending on the phone (SMS not saved in outbox). For Android < 4 or SmsToPhone as default SMS app
+	 * $hidden: boolean.
+	 * true: SMS not saved in outbox
+	 * false: SMS saved in outbox
+	 */
+	 public function setHidden($hidden) {
 		$this->hidden = $hidden;
 	}
 	
+	/**
+	 * Define sending rate to send messages.
+	 * $rate: integer. Number of message sent per minute
+	 * $rate=0: Auto - Rate will be determined depending on phone restriction
+	 */
+	public function setRate($rate) {
+		$this->rate = $rate;
+	}
+	
+	/**
+	 * Add a message to this bulk
+	 * $body: content of the message
+	 * $addresses: array or recipients
+	 */
 	public function addMessage($body, $addresses) {
 		$this->messages[] = new Sms($body, $addresses);
 	}
--- src/model/Sending.php
+++ src/model/Sending.php
@@ -2,20 +2,58 @@
 class Sending {
 	public $silent;
 	public $hidden;
+	public $rate = 0;
+	public $collapse = null;
 	public $message;
 	
+	/**
+	 * $body: content of the message
+	 * $addresses: array or recipients
+	 * $silent: see 'setSilent'
+	 * $hidden: see 'setHiden'
+	 */
 	function __construct($body, $addresses, $silent = false, $hidden = false) {
 		$this->message = new Sms($body, $addresses);
 		$this->silent = $silent;
 		$this->hidden = $hidden;
 	}
 	
-	public function setSilent($silent) {
+	/**
+	 * Silence sending on the phone (no notification)
+	 * $silent: boolean.
+	 * true: No notification
+	 * false: Each sending notified
+	 */
+	 public function setSilent($silent) {
 		$this->silent = $silent;
 	}
 	
-	public function setHidden($hidden) {
+	/**
+	 * Hide sending on the phone (SMS not saved in outbox). For Android < 4 or SmsToPhone as default SMS app
+	 * $hidden: boolean.
+	 * true: SMS not saved in outbox
+	 * false: SMS saved in outbox
+	 */
+	 public function setHidden($hidden) {
 		$this->hidden = $hidden;
+	}
+	
+	/**
+	 * Allows collapsing notification when sending multiple uniq messages
+	 * $collapse: string. Each sending with equals $collapse will be collapsed
+	 * $collapse=null: Notification not collapsed
+	 */
+	public function setCollapse($collapse) {
+		$this->collapse = $collapse;
+	}
+	
+	/**
+	 * Define sending rate to send messages.
+	 * $rate: integer. Number of message sent per minute
+	 * $rate=0: Auto - Rate will be determined depending on phone restriction
+	 */
+	public function setRate($rate) {
+		$this->rate = $rate;
 	}
 }
 ?>
